786357dde4c606e3898219361d901312983ce7b5
tyronefsn <tsnagano1@up.edu.ph>
2024-11-15 23:58:35 +0800

    docs: add comment

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $password = "";
 $dbname = "us_employees";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+// error handler
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
